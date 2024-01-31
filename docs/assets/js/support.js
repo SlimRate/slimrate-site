16992c4978bf96fffd2659607d4f4610edccade8
TheMarkest <markbogdanovofficial@gmail.com>
2024-01-31 23:31:58 +0600

    support fix

--- docs/assets/js/support.js
+++ docs/assets/js/support.js
@@ -1,4 +1,3 @@
-<script type="text/javascript">
   (function(w, d, x, id){
     s=d.createElement('script');
     s.src='https://db08fjupg2abb.cloudfront.net/amazon-connect-chat-interface-client.js';
@@ -10,4 +9,3 @@
   amazon_connect('styles', { iconType: 'CHAT', openChat: { color: '#ffffff', backgroundColor: '#EE2F2B' }, closeChat: { color: '#ffffff', backgroundColor: '#EE2F2B'} });
   amazon_connect('snippetId', 'QVFJREFIZ2JTNzdvNlZBdTFZSVJkT0xad3RDb08wM2NZamVYWno2UE4rbHpYL2JuRlFGOWhmUXA5TUE4TC9mU05uck8yMWdJQUFBQWJqQnNCZ2txaGtpRzl3MEJCd2FnWHpCZEFnRUFNRmdHQ1NxR1NJYjNEUUVIQVRBZUJnbGdoa2dCWlFNRUFTNHdFUVFNYW5tUXdqc2xxMno2Y0xZZEFnRVFnQ3NCOVBUZVRMUUJoSzM4KytZRGh6bXVROUVUaklpRm81Rm9rUDdRSHVkSjFZYVI5T2dpKzM1Q0ZucGg6Oi9zSklaaDZ1aGcyT1ZzditFRWJMUy8xOFBNT2toQjBETWJWbzdxRmc4UVNzWlRrMGovYy9RVmtJTWZtQnpySnFUWlhJR0w1Tk9xUmwwNERidXhrOFIzbXJFeHZIdnluYmxtdWEyTGFhWlBPUkxTNGNkeU9lOTF4cXdYcjgrZmFWbUY0RkwzSG5mQURTVm45a1l0WnIwVEJtcEZGWkZDaz0=');
   amazon_connect('supportedMessagingContentTypes', [ 'text/plain', 'text/markdown' ]);
-</script>
